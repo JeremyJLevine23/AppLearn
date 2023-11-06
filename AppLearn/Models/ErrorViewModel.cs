73fe38fc848a3dc2ec3a3451b12ab1189f00758f
JeremyJLevine23 <jeremylevine1020@gmail.com>
2023-11-06 17:55:37 -0600

    Add project files.

---
+++ AppLearn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppLearn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
